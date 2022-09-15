197558558bb7f972b98c7a0f08bfd506c2a71dad
lis7fire <las7fire@gmail.com>
2022-09-15 11:14:23 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# simulate_mall
+ 模拟电商商城
---
+++ src/main/java/com/bing/SimulateMallApplication.java
@@ -0,0 +1,13 @@
+package com.bing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimulateMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimulateMallApplication.class, args);
+    }
+
+}
